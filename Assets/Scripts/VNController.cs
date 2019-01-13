2503e2682241c201bbbde202032e493a67432708
tomual <tomual@users.noreply.github.com>
2019-01-13 16:48:07 +0800

    Next page dialogue

---
+++ Assets/Scripts/VNController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VNController : MonoBehaviour {
+
+    private Text scrollingText;
+
+	void Start () {
+        scrollingText = GameObject.FindGameObjectWithTag("ScrollingText").GetComponent<Text>();
+        InitConversation();
+	}
+	
+	void Update () {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            NextPage();
+        }
+    }
+
+    void InitConversation()
+    {
+        ChangeText("Page one");
+    }
+
+    void NextPage()
+    {
+        ChangeText("Page Two");
+    }
+
+    void ChangeText(string message)
+    {
+        scrollingText.text = message;
+    }
+}
