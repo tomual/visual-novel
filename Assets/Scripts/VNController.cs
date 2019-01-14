725517eb8236e2dadcf14dfccaf142823ae37a80
tomual <tomual@users.noreply.github.com>
2019-01-14 16:16:38 +0800

    Line class, highlight talking character

--- Assets/Scripts/VNController.cs
+++ Assets/Scripts/VNController.cs
@@ -7,7 +7,7 @@ public class VNController : MonoBehaviour {
 
     private Text textComponent;
 
-    private string[] dialogue;
+    private List<Line> dialogue;
 
     private bool scrolling;
     private float scrollSpeed = 0.02f;
@@ -15,15 +15,25 @@ public class VNController : MonoBehaviour {
 
     private string text;
     private int scrollingCursor;
+    private int lineCursor = 0;
 
-	void Start ()
+    private GameObject characterRight;
+    private GameObject characterLeft;
+
+    void Start ()
     {
-        dialogue = new string[] {
-            "Page One",
-            "Page Two"
-        };
+        dialogue = new List<Line>();
+        dialogue.Add(new Line(Position.LEFT, "Hello, I'm a merman. I am half fish and half man."));
+        dialogue.Add(new Line(Position.RIGHT, "Hello, I'm a mermaid. I am half man and half fish."));
+        dialogue.Add(new Line(Position.LEFT, "Jolly good."));
+        dialogue.Add(new Line(Position.RIGHT, "What say you do a cup of tea at Margaret's Cafe?"));
+        dialogue.Add(new Line(Position.LEFT, "It sounds most enjoyable."));
 
         textComponent = GameObject.FindGameObjectWithTag("ScrollingText").GetComponent<Text>();
+
+        characterRight = GameObject.FindGameObjectWithTag("CharacterRight");
+        characterLeft = GameObject.FindGameObjectWithTag("CharacterLeft");
+
         InitConversation();
     }
 	
@@ -50,12 +60,26 @@ public class VNController : MonoBehaviour {
 
     void InitConversation()
     {
-        ChangeText(dialogue[0]);
+        NextPage();
     }
 
     void NextPage()
     {
-        ChangeText(dialogue[1]);
+        Line line = dialogue[lineCursor];
+        Debug.Log(line.Position);
+        ChangeText(line.Message);
+        if (line.Position == Position.LEFT)
+        {
+            Debug.Log(characterRight);
+            characterRight.GetComponent<RawImage>().color = new Color32(126, 143, 156, 255);
+            characterLeft.GetComponent<RawImage>().color = new Color32(255, 255, 255, 255);
+        } else
+        {
+            Debug.Log(characterLeft);
+            characterLeft.GetComponent<RawImage>().color = new Color32(126, 143, 156, 255);
+            characterRight.GetComponent<RawImage>().color = new Color32(255, 255, 255, 255);
+        }
+        lineCursor++;
     }
 
     void ChangeText(string message)
@@ -66,4 +90,18 @@ public class VNController : MonoBehaviour {
         scrollingCursor = 0;
         lastPrint = Time.time - scrollSpeed;
     }
+}
+
+public enum Position { LEFT, RIGHT };
+
+public class Line {
+    public Position Position;
+    public string Message;
+
+    public Line(Position position, string message)
+    {
+        Position = position;
+        Message = message;
+    }
+
 }
